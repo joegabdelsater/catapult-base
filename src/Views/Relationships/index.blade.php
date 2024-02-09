a93fe787ab35fbbbf3ff1131709f3665b866b27c
Joe Abdel Sater <joegabdelsater@gmail.com>
2024-02-09 20:08:27 +0200

    add status

--- src/Views/Relationships/index.blade.php
+++ src/Views/Relationships/index.blade.php
@@ -7,7 +7,16 @@
 
         @foreach ($models as $model)
             <a href="{{ route('catapult.relationships.create', ['modelId' => $model->id]) }}"
-                class="text-gray-700 block font-bold model hover:text-white mb-16 border-2 border-dashed hover:border-solid p-4 rounded-md mb-4 hover:bg-gray-700">{{ $model->name }}.php</a>
+                class="text-gray-700 block font-bold model group hover:text-white mb-16 border-2 border-dashed hover:border-solid p-4 rounded-md mb-4 hover:bg-gray-700 flex flex-row justify-between items-center"><span>{{ $model->name }}.php</span>
+                <div class="flex flex-row items-center">
+                    @if ($model->relationships->count() > 0)
+                        <span class="text-xs text-gray-500 mr-2 group-hover:text-white">({{ $model->relationships->count() }} relationships)</span>
+
+                        @component('catapult::components.icons.checkmark-circle', ['class' => 'h-5 w-5', 'fill' => 'green'])
+                        @endcomponent
+                    @endif
+                </div>
+            </a>
         @endforeach
 
 
