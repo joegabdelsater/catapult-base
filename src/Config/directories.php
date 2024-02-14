a117667e4d1087db9916dcd0c61552fbcaea019e
Joe Abdel Sater <joegabdelsater@gmail.com>
2024-02-14 12:10:49 +0200

    add controller routes

--- src/Config/directories.php
+++ src/Config/directories.php
@@ -3,6 +3,7 @@
 return [
     'models' => __DIR__ . '/../../../../../app/Models',
     'validation_requests' => __DIR__ . '/../../../../../app/Http/Requests/',
+    'controllers' => __DIR__ . '/../../../../../app/Http/Controllers/',
     'temp_migrations' =>  __DIR__ . '/../Temp',
     'migrations' => __DIR__ . '/../../../../../database/migrations',
     'resources' => ''
