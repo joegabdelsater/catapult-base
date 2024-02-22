5981cc2f36def6ceadeb828503502f44237959c7
Joe Abdel Sater <joegabdelsater@gmail.com>
2024-02-22 19:17:55 +0200

    Fix path after moving provider

--- src/CatapultBaseServiceProvider.php
+++ src/CatapultBaseServiceProvider.php
@@ -16,14 +16,14 @@ class CatapultBaseServiceProvider extends ServiceProvider
     /** @todo add instructions to publish js */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/../views', 'catapult');
-        $this->loadMigrationsFrom([__DIR__ . '/../migrations']);
-        $this->mergeConfigFrom(__DIR__ . '/../config/relationships.php', 'relationships');
-        $this->mergeConfigFrom(__DIR__ . '/../config/directories.php', 'directories');
-        $this->mergeConfigFrom(__DIR__ . '/../config/migrations.php', 'migrations');
-        $this->mergeConfigFrom(__DIR__ . '/../config/packages.php', 'packages');
-        $this->mergeConfigFrom(__DIR__ . '/../config/routes.php', 'routes');
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/views', 'catapult');
+        $this->loadMigrationsFrom([__DIR__ . '/migrations']);
+        $this->mergeConfigFrom(__DIR__ . '/config/relationships.php', 'relationships');
+        $this->mergeConfigFrom(__DIR__ . '/config/directories.php', 'directories');
+        $this->mergeConfigFrom(__DIR__ . '/config/migrations.php', 'migrations');
+        $this->mergeConfigFrom(__DIR__ . '/config/packages.php', 'packages');
+        $this->mergeConfigFrom(__DIR__ . '/config/routes.php', 'routes');
 
         if ($this->app->runningInConsole()) {
             $this->commands([
@@ -32,7 +32,7 @@ class CatapultBaseServiceProvider extends ServiceProvider
         }
 
         $this->publishes([
-            __DIR__ . '/../assets/js/' => public_path('Joeabdelsater/catapult-base/js/'),
+            __DIR__ . '/assets/js/' => public_path('Joeabdelsater/catapult-base/js/'),
         ], 'catapult-base');
 
         if (!app()->runningInConsole()) {
@@ -44,6 +44,6 @@ class CatapultBaseServiceProvider extends ServiceProvider
                 ]
             ]);
         }
-
+        
     }
 }
